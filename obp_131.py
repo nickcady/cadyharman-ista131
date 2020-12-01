6b3e8f1702bfd910ad8f97a7e7950d0e5178ac6a
Nick Cady <ncady711@gmail.com>
2020-11-30 20:03:00 -0700

    Tommy's git was malfunctioning. Adds slug and strikeout graph

--- obp_131.py
+++ obp_131.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib as mpl
 import statsmodels.api as sm
 import math
-df = pd.read_csv ('baseball.csv')
+df = pd.read_csv ('teamdata.csv')
 
 y = df['R']
 x = df['OBP']
@@ -20,6 +20,7 @@ print(results.params)
 
 y_hat=results.predict(x_1)
 plt.scatter(x,y)
+plt.title('On Base Percentage vs Runs')
 plt.xlabel("OBP")
 plt.ylabel("Runs")
 plt.plot(x,y_hat, "r")
